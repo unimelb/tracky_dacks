977bac4da0e44946cc700d9470c352a1a71a5fbd
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2017-04-26 16:38:15 +1000

    Extract Staccato from TrackyDack::Handlers::GA class

--- lib/tracky_dacks/handlers/ga.rb
+++ lib/tracky_dacks/handlers/ga.rb
@@ -1,27 +1,15 @@
-require "staccato"
 
 module TrackyDacks
   module Handlers
     class GA
       attr_reader :tracker
 
-      def initialize(tracker_options = {})
-        @tracker = build_tracker(tracker_options)
+      def initialize(tracker)
+        @tracker = tracker
       end
 
       def call(params = {})
         raise NotImplementedError
-      end
-
-      private
-
-      def build_tracker(options)
-        options = options.dup
-
-        tracking_id = options.delete(:tracking_id)
-        client_id = options.delete(:client_id)
-
-        Staccato.tracker(tracking_id, client_id, options)
       end
     end
   end
